1ebf1fa2fa9098d59f5bc1de13a0bc65e09d8c29
Kokorin <xx.artem-kokorin@mail.ru>
2019-12-06 21:20:13 +0300

    byOne

--- Header.h
+++ Header.h
@@ -1,1 +1,11 @@
 #pragma once
+#include <deque>
+#include <functional>
+
+template <typename Element>
+void byOne(std::deque<Element> & deq, std::function<void(Element)> f) {
+  while (!deq.empty()) {
+    f(deq.front());
+    deq.pop_front();
+  }
+}
